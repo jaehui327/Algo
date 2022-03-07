03ea56d149d97c0568b031ba42cbe6d336d4168d
zhilogs <jaehui327@naver.com>
2022-03-07 17:51:48 +0900

    [level 1] Title: K번째수, Time: 0.21 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.swift
+++ 프로그래머스/lv1/42748. K번째수/K번째수.swift
@@ -1,19 +1,23 @@
 import Foundation
 
 func solution(_ array:[Int], _ commands:[[Int]]) -> [Int] {
-    var result: [Int] = []
+   var result: [Int] = []
     for command in commands {
+        let i = command[0] - 1
+        let j = command[1] - 1
+        let k = command[2] - 1
+        
         var arr: [Int] = []
-        if command[0] == command[1] {
-            arr.append(array[command[0] - 1])
+        if i == j {
+            arr.append(array[i])
         }  else {
-            arr.append(contentsOf: array[command[0] - 1 ..< command[1]])
+            arr.append(contentsOf: array[i ... j])
         }
         arr.sort()
-        if arr.count < command[2] {
+        if arr.count < k {
             result.append(arr.last!)
         } else {
-            result.append(arr[command[2] - 1])
+            result.append(arr[k])
         }
     }
     return result
--- 프로그래머스/lv1/42748. K번째수/README.md
+++ 프로그래머스/lv1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/42748?language=swift) 
 
 ### 성능 요약
 
-메모리: 16.7 MB, 시간: 0.15 ms
+메모리: 16.3 MB, 시간: 0.21 ms
 
 ### 구분
 
