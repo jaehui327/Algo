2cbaeb4b738055d9175747d2b5f48bea430997f2
zhilogs <jaehui327@naver.com>
2022-04-28 19:21:43 +0900

    [Silver IV] Title: 큐, Time: 32 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.swift
+++ 백준/Silver/10845. 큐/큐.swift
@@ -5,25 +5,19 @@ var index = 0
 
 for _ in 0..<Int(readLine()!)! {
     let line = readLine()!.split(separator: " ").map { String($0) }
-    let size = queue.count - index
     switch line[0] {
     case "push":
         queue.append(Int(line[1])!)
     case "pop":
-        if size == 0 {
-            print(-1)
-        } else {
-            print(queue[index])
-            index += 1
-        }
+        print(queue.isEmpty ? -1 : queue.removeFirst())
     case "size":
-        print(size)
+        print(queue.count)
     case "empty":
-        print(size == 0 ? 1 : 0)
+        print(queue.isEmpty ? 1 : 0)
     case "front":
-        print(size == 0 ? -1 : queue[index])
+        print(queue.isEmpty ? -1 : queue[index])
     case "back":
-        print(size == 0 ? -1 : queue[queue.count - 1])
+        print(queue.isEmpty ? -1 : queue[queue.count - 1])
     default: break
     }
 }
