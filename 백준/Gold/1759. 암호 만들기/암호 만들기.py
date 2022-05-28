53669aa32999340457b2eb9570f00bac97d4ed12
zhilogs <jaehui327@naver.com>
2022-05-29 03:04:41 +0900

    [Gold V] Title: 암호 만들기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -5,11 +5,11 @@ line = list(map(int, stdin.readline().split()))
 L = line[0]
 arr = sorted(list(map(str, stdin.readline().split())))
 
-answer = list(combinations(arr, L))
-for a in answer:
-    cnt_vow = 0
-    for i in a:
-        if i in "aeiou":
-            cnt_vow += 1
-    if cnt_vow >= 1 and L - cnt_vow >= 2:
-        print("".join(a))
+com = list(combinations(arr, L))
+for c in com:
+	cnt_vow = 0
+	for i in c:
+		if i in "aeiou":
+			cnt_vow += 1
+	if cnt_vow >= 1 and L - cnt_vow >= 2:
+		print("".join(c))
