434e009e901c161f748173bc11b02e6696594602
zhilogs <jaehui327@naver.com>
2022-06-17 17:51:44 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 576 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.swift
@@ -1,23 +1,16 @@
 import Foundation
 
-func hanoi(_ begin: Int, _ mid: Int, _ end: Int, _ height: Int) {
-    if height == 1 {
-        move(begin, end)
-    } else {
-        hanoi(begin, end, mid, height - 1)
-        move(begin, end)
-        hanoi(mid, begin, end, height - 1)
+let K = Int(readLine()!)!
+
+func hanoi(_ current: Int, _ next: Int, _ k: Int) {
+    if k == 1 {
+        print("\(current) \(next)")
+        return
     }
+    hanoi(current, 6 - current - next, k - 1)
+    print("\(current) \(next)")
+    hanoi(6 - current - next, next, k - 1)
 }
 
-func move(_ begin: Int, _ end: Int) {
-    print("\(begin) \(end)")
-}
-
-let height = Int(readLine()!)!
-var result = 1
-for _ in 0 ..< height {
-    result *= 2
-}
-print(result - 1)
-hanoi(1, 2, 3, height)
+print(pow(2, K) - 1)
+hanoi(1, 3, K)
