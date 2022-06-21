a5a34598a9af2b055040dc0e296cda0da70673b5
zhilogs <jaehui327@naver.com>
2022-06-21 17:02:16 +0900

    [Silver II] Title: 종이의 개수, Time: 1368 ms, Memory: 117300 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.swift
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.swift
@@ -8,20 +8,28 @@ for _ in 0..<n {
 
 var count = [0, 0, 0] // -1,0,1로 채워진 종이 개수
 
-func paper(_ x: Int, _ y: Int, _ width: Int) {
+func check(_ x: Int, _ y: Int, _ width: Int) -> Bool {
     for i in x..<x + width {
         for j in y..<y + width {
             if board[i][j] != board[x][y] {
-                for k in 0..<3 {
-                    for l in 0..<3 {
-                        paper(x + k * (width / 3), y + l * (width / 3), width / 3)
-                    }
-                }
-                return
+                return false
             }
         }
     }
-    count[board[x][y] + 1] += 1
+    return true
+}
+
+func paper(_ x: Int, _ y: Int, _ width: Int) {
+    if check(x, y, width) {
+        count[board[x][y] + 1] += 1
+        return
+    }
+    let w = width / 3
+    for i in 0..<3 {
+        for j in 0..<3 {
+            paper(x + i * w, y + j * w, w)
+        }
+    }
 }
 
 paper( 0, 0, n)
