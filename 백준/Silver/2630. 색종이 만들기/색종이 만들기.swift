1ba72a4d3466b75a52913b06b07ec2f5b1e9cabb
zhilogs <jaehui327@naver.com>
2022-06-22 16:45:49 +0900

    [Silver II] Title: 색종이 만들기, Time: 16 ms, Memory: 79644 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+let n = Int(readLine()!)!
+var board = [[Int]]()
+for _ in 0..<n {
+    board.append(readLine()!.split(separator: " ").map { Int(String($0))! })
+}
+var count = [0, 0]
+
+func check(_ x: Int, _ y: Int, _ width: Int) -> Bool {
+    for i in x..<x + width {
+        for j in y..<y + width {
+            if board[x][y] != board[i][j] {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+func confetti(_ x: Int, _ y: Int, _ width: Int) {
+    if check(x, y, width) {
+        count[board[x][y]] += 1
+        return
+    }
+    let w = width / 2
+    for i in 0..<2 {
+        for j in 0..<2 {
+            confetti(x + i * w, y + j * w, w)
+        }
+    }
+}
+
+confetti(0, 0, n)
+for c in count {
+    print(c)
+}
