75d3b679f998eaa45805d066416225ac4486b698
zhilogs <jaehui327@naver.com>
2022-07-17 22:45:28 +0900

    [D2] Title: 파리 퇴치, Time: 153 ms, Memory: 24,252 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.java
@@ -74,8 +74,8 @@ class Solution
 			}
 			
 			int answer = 0;
-			for(int i = 0; i < N - M; i++) {
-				for(int j = 0; j < N - M; j++) {
+			for(int i = 0; i < N - M + 1; i++) {
+				for(int j = 0; j < N - M + 1; j++) {
 					int temp = 0;
 					for(int a = 0; a < M; a++) {
 						for(int b = 0; b < M; b++) {
