bb233fbedcfbd80577223c45765d159671a31d9f
zhilogs <jaehui327@naver.com>
2022-08-02 18:04:37 +0900

    [D2] Title: 달팽이 숫자, Time: 103 ms, Memory: 18,656 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("input/1954.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int[][] dir = {{0,1}, {1,0}, {0,-1}, {-1, 0}};
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int t=1; t<=T; t++) {
+			
+			int N = Integer.parseInt(br.readLine());
+			int[][] board = new int[N][N];
+			int[][] visited = new int[N][N];
+			
+			int num = 1;
+			int d=0;
+			int x=0;
+			int y=0;
+			board[x][y] = num++;
+			visited[x][y] = 1;
+			
+			while(true) {
+				if(num>N*N) break;
+				int nx = x + dir[d][0];
+				int ny = y + dir[d][1];
+				if(nx>=0 && nx<N && ny>=0 && ny<N && visited[nx][ny]==0) {
+					board[nx][ny] = num++;
+					visited[nx][ny] = 1;
+					x = nx;
+					y = ny;
+				} else {
+					d = (d+1)%4;
+					x += dir[d][0];
+					y += dir[d][1];
+					board[x][y] = num++;
+					visited[x][y] = 1;
+					
+				}
+			}
+			
+			sb.append("#");
+			sb.append(t);
+			sb.append("\n");
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					sb.append(board[i][j]);
+					sb.append(" ");
+				}
+				sb.append("\n");
+			}
+		}
+		
+		br.close();
+		System.out.println(sb);
+	}
+
+}
