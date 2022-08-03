27e8d2d2ee9ea160671bcb7c6dda2a569074bb2f
zhilogs <jaehui327@naver.com>
2022-08-03 12:59:24 +0900

    [D3] Title: 상호의 배틀필드, Time: 115 ms, Memory: 21,948 KB -BaekjoonHub

--- SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
+++ SWEA/D3/1873. 상호의 배틀필드/상호의 배틀필드.java
@@ -7,6 +7,52 @@ import java.util.StringTokenizer;
 // https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LyE7KD2ADFAXc
 public class Solution {
 
+	static char[][] board = new char[20][20];
+	static int H, W;
+	static char[] s = new char[100];
+
+	static int x = 0;
+	static int y = 0;
+	static int[][] dir = { { -1, 0, '^', 'U' }, { 1, 0, 'v', 'D' }, { 0, -1, '<', 'L' }, { 0, 1, '>', 'R'} };
+	static int d = 0;
+
+	static void shoot() {
+		for(int i=0; i<4; i++) {
+			if((char)dir[i][2]==board[x][y])
+				d = i;
+		}
+		int nx = x, ny = y;
+		while (true) {
+			nx += dir[d][0];
+			ny += dir[d][1];
+			if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
+				if (board[nx][ny] == '*') {
+					board[nx][ny] = '.';
+					return;
+				} else if (board[nx][ny] == '#')
+					return;
+			} else
+				return;
+		}
+	}
+
+	static void move(char D) {
+		for(int i=0; i<4; i++) {
+			if((char)dir[i][3]==D)
+				d = i;
+		}
+		board[x][y] = (char)dir[d][2];
+
+		int nx = x + dir[d][0];
+		int ny = y + dir[d][1];
+		if (nx >= 0 && nx < H && ny >= 0 && ny < W && board[nx][ny] == '.') {
+			board[x][y] = '.';
+			x = nx;
+			y = ny;
+			board[x][y] = (char)dir[d][2];
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 //		System.setIn(new FileInputStream("input/1873.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,15 +61,8 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int H = Integer.parseInt(st.nextToken());
-			int W = Integer.parseInt(st.nextToken());
-
-			char[][] board = new char[H][W];
-			int x = 0;
-			int y = 0;
-
-			int[][] dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-			int d = 0;
+			H = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
 
 			for (int i = 0; i < H; i++) {
 				String s = br.readLine();
@@ -31,70 +70,18 @@ public class Solution {
 					char c = s.charAt(j);
 					board[i][j] = c;
 					if (c == '^' || c == 'v' || c == '<' || c == '>') {
-						x = i;
-						y = j;
-						if (c == '^')
-							d = 0;
-						else if (c == 'v')
-							d = 1;
-						else if (c == '<')
-							d = 2;
-						else
-							d = 3;
+						x = i; y = j;
 					}
-
 				}
 			}
 
 			int N = Integer.parseInt(br.readLine());
-			char[] s = br.readLine().toCharArray();
+			s = br.readLine().toCharArray();
 			for (int i = 0; i < N; i++) {
 				if (s[i] == 'S') { // 'S', 현재 방향으로 포탄 발사
-					int nx = x;
-					int ny = y;
-					while (true) {
-						nx += dir[d][0];
-						ny += dir[d][1];
-						if (nx >= 0 && nx < H && ny >= 0 && ny < W) {
-							if (board[nx][ny] == '*') {
-								board[nx][ny] = '.';
-								break;
-							} else if (board[nx][ny] == '#')
-								break;
-						} else
-							break;
-					}
+					shoot();
 				} else {
-					if (s[i] == 'U') {
-						d = 0;
-						board[x][y] = '^';
-					} else if (s[i] == 'D') {
-						d = 1;
-						board[x][y] = 'v';
-					} else if (s[i] == 'L') {
-						d = 2;
-						board[x][y] = '<';
-					} else if (s[i] == 'R') {
-						d = 3;
-						board[x][y] = '>';
-					}
-					
-					int nx = x + dir[d][0];
-					int ny = y + dir[d][1];
-					if (nx >= 0 && nx < H && ny >= 0 && ny < W && board[nx][ny] == '.') {
-						board[x][y] = '.';
-						x = nx;
-						y = ny;
-						if (s[i] == 'U')
-							board[x][y] = '^';
-						else if (s[i] == 'D')
-							board[x][y] = 'v';
-						else if (s[i] == 'L')
-							board[x][y] = '<';
-						else if (s[i] == 'R')
-							board[x][y] = '>';
-
-					}
+					move(s[i]);
 				}
 			}
 
