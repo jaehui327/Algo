0a34e962904d966af51a6a190123380aa1e06f6b
zhilogs <jaehui327@naver.com>
2022-08-04 13:03:08 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 128 ms, Memory: 18,436 KB -BaekjoonHub

---
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Stack;
+
+//1218. 괄호 짝짓기
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14eWb6AAkCFAYD
+	
+public class Solution {
+	
+	static HashMap<Character, Character> map = new HashMap<Character, Character>() {{
+		put('(', ')'); put('[', ']'); put('{', '}'); put('<', '>');
+	}};
+	
+	static int isValidBracket(int N, char[] bracket) {
+		Stack<Character> stack = new Stack<Character>();
+		for(int i=0; i<N; i++) {
+			if(map.containsKey(bracket[i])) stack.push(bracket[i]);
+			else {
+				if(bracket[i] == map.get(stack.peek())) stack.pop();
+				else return 0;
+			}
+		}
+		return 1;
+	}
+
+	public static void main(String[] args) throws Exception {
+//		System.setIn(new FileInputStream("input/1218.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+
+		for (int t = 1; t <= 10; t++) {
+			int N = Integer.parseInt(br.readLine());
+			char[] bracket = br.readLine().toCharArray();
+			
+			sb.append("#");
+			sb.append(t);
+			sb.append(" ");
+			sb.append(isValidBracket(N, bracket));
+			sb.append("\n");
+		}
+
+		br.close();
+		System.out.println(sb);
+	}
+
+}
