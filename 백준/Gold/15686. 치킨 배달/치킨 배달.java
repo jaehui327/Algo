e23b0d7c7509add59c72e84004909cfad48bdbd3
zhilogs <jaehui327@naver.com>
2022-08-12 14:39:16 +0900

    [Gold V] Title: 치킨 배달, Time: 204 ms, Memory: 15100 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.java
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.java
@@ -7,10 +7,13 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+// 치킨 배달
+// https://www.acmicpc.net/problem/15686
 public class Main {
 
 	static int N, M, min;
-	static int[][] board, selected, dir = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	static int[] selected;
+	static int[][] board;
 	static ArrayList<int[]> house, chiken;
 
 	private static void chiken_distance(int idx, int start) {
@@ -19,41 +22,21 @@ public class Main {
 			return;
 		}
 		for (int i = start; i < chiken.size(); i++) {
-			selected[idx] = chiken.get(i);
+			selected[idx] = i;
 			chiken_distance(idx + 1, i + 1);
 		}
 	}
 
 	private static void get_distance() {
-		int[][] board = new int[N + 1][N + 1];
-		int temp = 0;
-		for (int i = 0; i < selected.length; i++) {
-			board[selected[i][0]][selected[i][1]] = 2;
-		}
+		int sum = 0;
 		for (int i = 0; i < house.size(); i++) {
-			int[][] visited = new int[N + 1][N + 1];
-			Queue<int[]> queue = new LinkedList<>();
-			int[] t = house.get(i);
-			visited[t[0]][t[1]] = 1;
-			queue.offer(new int[] {t[0], t[1]});
-			outer:while(!queue.isEmpty()) {
-				int[] h = queue.poll();
-				int r = h[0];
-				int c = h[1];
-				for (int d = 0; d < 4; d++) {
-					int nr = r + dir[d][0];
-					int nc = c + dir[d][1];
-					if (nr > 0 && nr <= N && nc > 0 && nc <= N && visited[nr][nc] == 0) {
-						visited[nr][nc] = visited[r][c] + 1;
-						if (board[nr][nc] == 2) {
-							temp += visited[r][c];
-							break outer;
-						} else queue.offer(new int[] {nr, nc});
-					}
-				}
+			int temp = Integer.MAX_VALUE;
+			for (int j = 0; j < selected.length; j++) {
+				temp = Math.min(temp, Math.abs(house.get(i)[0] - chiken.get(selected[j])[0]) + Math.abs(house.get(i)[1] - chiken.get(selected[j])[1]));
 			}
+			sum += temp;
 		}
-		min = Math.min(min, temp);
+		min = Math.min(min, sum);
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -63,14 +46,16 @@ public class Main {
 		StringTokenizer s = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(s.nextToken());
 		M = Integer.parseInt(s.nextToken());
+		
 		min = Integer.MAX_VALUE;
-		board = new int[N + 1][N + 1];
-		selected = new int[M][2];
+		board = new int[N ][N];
+		selected = new int[M];
 		house = new ArrayList<>();
 		chiken = new ArrayList<>();
-		for (int i = 1; i <= N; i++) {
+		
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 1; j <= N; j++) {
+			for (int j = 0 ; j < N; j++) {
 				board[i][j] = Integer.parseInt(st.nextToken());
 				if (board[i][j] == 1)
 					house.add(new int[] { i, j });
