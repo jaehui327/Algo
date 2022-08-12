425e845550458fe71cd430c51d08f2336933bbbd
zhilogs <jaehui327@naver.com>
2022-08-12 17:19:51 +0900

    [Silver II] Title: 스타트와 링크, Time: 356 ms, Memory: 18304 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -3,10 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 스타트와 링크
+// https://www.acmicpc.net/problem/14889
 public class Main {
 	
-	static int N, min;
-	static boolean[] selected;
+	static int N, min,  s, o;
+	static int[] selected, others, xy;
 	static int[][] board;
 	
 	private static void startLink(int idx, int start) {
@@ -15,32 +17,58 @@ public class Main {
 			return;
 		}
 		for (int i = start; i < N; i++) {
-			selected[i] = true;
+			selected[idx] = i;
 			startLink(idx + 1, i + 1);
-			selected[i] = false;
 		}
 	}
 	
 	private static void get_power() {
-		int S = 0, L = 0;
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = i + 1; j < N; j++) {
-				if (selected[i] && selected[j] && i != j) 
-					S += board[i][j] + board[j][i];
-				else if (!selected[i] && !selected[j] && i != j)
-					L += board[i][j] + board[j][i];
+		int s_i = 0, o_i = 0;
+		for (int i = 0; i < N; i++) {
+			if(s_i < N / 2 && selected[s_i] == i) {
+				s_i++;
+			} else {
+				others[o_i++] = i;
 			}
 		}
-		min = Math.min(min, Math.abs(S - L));
+		s = 0; o = 0;
+		get_sum_selected(0, 0);
+		get_sum_others(0, 0);
+		min = Math.min(min, Math.abs(s - o));
+	}
+	
+	private static void get_sum_selected(int idx, int start) {
+		if (idx == 2) {
+			s += board[xy[0]][xy[1]] + board[xy[1]][xy[0]];
+			return;
+		}
+		for (int i = start; i < N / 2; i++) {
+			xy[idx] = selected[i];
+			get_sum_selected(idx + 1, i + 1);
+		}
+	}
+	
+	private static void get_sum_others(int idx, int start) {
+		if (idx == 2) {
+			o += board[xy[0]][xy[1]] + board[xy[1]][xy[0]];
+			return;
+		}
+		for (int i = start; i < N / 2; i++) {
+			xy[idx] = others[i];
+			get_sum_others(idx + 1, i + 1);
+		}
 	}
  	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
+		
 		N = Integer.parseInt(br.readLine());
 		min = Integer.MAX_VALUE;
-		selected = new boolean[N];
+		board = new int[N][N];
+		selected = new int[N / 2];
+		others = new int[N / 2];
+		xy = new int[2];
 		
 		board = new int[N][N];
 		
