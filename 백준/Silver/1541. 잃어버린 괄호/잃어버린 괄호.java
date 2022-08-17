65f90e3399d93a1eb296ad301b46b198d8ed8708
zhilogs <jaehui327@naver.com>
2022-08-17 13:35:17 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -2,36 +2,27 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static Stack<String> stack;
-	
-	private static int makeMin() {
-		int result = 0;
-		int temp = 0;
-		while(!stack.isEmpty()) {
-			String s = stack.pop();
-			if (s.equals("-")) {
-				result -= temp;
-				temp = 0;
-			} else if (!s.equals("+")) {
-				temp += Integer.parseInt(s);
-			}
-		}
-		return result + temp;
-	}
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		stack = new Stack<>();
-		String[] s = br.readLine().split("(?<=[+-])|(?=[+-])");
-		for (int i = 0; i < s.length; i++) {
-			stack.add(s[i]);
+		String s = br.readLine();
+		StringTokenizer st = new StringTokenizer(s, "+-");
+		int min = Integer.parseInt(st.nextToken());
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == '-') {
+				break;
+			} else if (s.charAt(i) == '+') {
+				min += Integer.parseInt(st.nextToken());
+			}
 		}
-		sb.append(makeMin());
+		while (st.hasMoreTokens())
+			min -= Integer.parseInt(st.nextToken());
+		sb.append(min);
 		
 		br.close();
 		System.out.println(sb);
