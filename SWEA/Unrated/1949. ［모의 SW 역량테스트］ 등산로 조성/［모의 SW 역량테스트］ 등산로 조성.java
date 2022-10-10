2d34bb125a54721c90de6e65d97c9bf1a3d9ce81
zhilogs <jaehui327@naver.com>
2022-10-10 19:00:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 등산로 조성, Time: 181 ms, Memory: 31,876 KB -BaekjoonHub

--- SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
+++ SWEA/Unrated/1949. ［모의 SW 역량테스트］ 등산로 조성/［모의 SW 역량테스트］ 등산로 조성.java
@@ -3,93 +3,97 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
 	
 	static class Position {
-		int y;
 		int x;
+		int y;
 		
-		Position(int y, int x) {
-			this.y = y;
+		Position(int x, int y) {
 			this.x = x;
+			this.y = y;
 		}
 	}
-	
-	static int N, K, max, highest;
-	static int[] dy = { -1 , 0, 1, 0 }, dx = { 0, 1, 0, -1 };
+
+	static int N, K, maxHeight, answer, cnt;
+	static int[] dx = { 1, 0, -1 ,0 }, dy = { 0, -1, 0, 1 };
 	static int[][] board;
 	static boolean[][] visited;
-	static ArrayList<Position> peaks;
+	static List<Position> peaks;
 	
-	private static void hikingTrail() {
-		for (int y = 0; y < N; y++) {
-			for (int x = 0; x < N; x++) {
-				int temp = board[y][x];
-				for (int k = 0; k <= K; k++) {
-					board[y][x] = temp - k;
-					move();
-				}
-				board[y][x] = temp;
-			}
-		}
-	}
-	
-	private static void move() {
-		for (Position peak : peaks) {
+	private static void construction(int x, int y, int k) {
+		int height = board[y][x];
+		board[y][x] -= k;
+		for (int i = 0; i < peaks.size(); i++) {
+			cnt = 0;
 			visited = new boolean[N][N];
-			visited[peak.y][peak.x] = true;
-			dfs(peak.y, peak.x, 1);
+			dfs(peaks.get(i).x, peaks.get(i).y, visited);
 		}
+		board[y][x] = height;
 	}
 	
-	private static void dfs(int y, int x, int route) {
-		max = Math.max(max, route);
+	private static void dfs(int x, int y, boolean[][] visited) {
+		visited[y][x] = true;
+		cnt += 1;
+		answer = Math.max(answer, cnt);
+		int nx, ny;
 		for (int d = 0; d < 4; d++) {
-			int ny = y + dy[d];
-			int nx = x + dx[d];
-			if (ny >= 0 && ny < N && nx >= 0 && nx < N && !visited[ny][nx] && board[ny][nx] < board[y][x]) {
-				visited[ny][nx] = true;
-				dfs(ny, nx, route + 1);
-				visited[ny][nx] = false;
-			}
+			nx = x + dx[d];
+			ny = y + dy[d];
+			if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+			if (visited[ny][nx] || board[ny][nx] >= board[y][x]) continue;
+			dfs(nx, ny, visited);
 		}
+		visited[y][x] = false;
+		cnt -= 1;
 	}
 	
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
-
+		StringTokenizer st = null;
+		
 		int T = Integer.parseInt(br.readLine());
 		for (int t = 1; t <= T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
+			st = new StringTokenizer(br.readLine(), " ");
 			N = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-			max = 0;
-			highest = 0;
+			maxHeight = 0;
+			answer = 0;
 			
 			board = new int[N][N];
-			peaks = new ArrayList<>();
 			
-			for (int y = 0; y < N; y++) {
-				StringTokenizer s = new StringTokenizer(br.readLine());
-				for (int x = 0; x < N; x++) {
-					board[y][x] = Integer.parseInt(s.nextToken());
-					if (board[y][x] > highest) {
-						highest = board[y][x];
-						peaks.clear();
-						peaks.add(new Position(y, x));
-					} else if (board[y][x] == highest) peaks.add(new Position(y, x));
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+					if (maxHeight < board[i][j]) {
+						maxHeight = board[i][j];
+						peaks = new ArrayList<>();
+					}
+					if (maxHeight == board[i][j])
+						peaks.add(new Position(j, i));
 				}
 			}
-
-			hikingTrail();
-			sb.append("#").append(t).append(" ").append(max).append("\n");
+			
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					for (int k = 1; k <= K; k++) {
+						construction(j, i, k);
+					}
+				}
+			}
+			
+			sb.append("#").append(t).append(" ");
+			sb.append(answer).append("\n");
 		}
-
+		
+		
 		br.close();
-		System.out.println(sb);
+		System.out.println(sb.toString());
 	}
+
 }
