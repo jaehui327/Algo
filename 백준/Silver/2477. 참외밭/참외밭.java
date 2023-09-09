de30e46b486ec1560195ae5eebbc1c6d483bb1e0
zhilogs <jaehui327@naver.com>
2023-09-09 16:04:13 +0900

    [Silver II] Title: 참외밭, Time: 76 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Silver/2477. 참외밭/참외밭.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+		int K = Integer.parseInt(st.nextToken());
+		int hMax = 0, wMax = 0; 
+		int hMaxIdx = -1, wMaxIdx = -1; 
+		int[] dirs = new int[6]; 
+		int[] dist = new int[6]; 
+ 
+		for (int i = 0; i < 6; i++) {
+			st = new StringTokenizer(br.readLine());
+			dirs[i] = Integer.parseInt(st.nextToken());
+			dist[i] = Integer.parseInt(st.nextToken());
+			if (dirs[i] == 1 || dirs[i] == 2) { 
+				if (hMax < dist[i]) { 
+ 					hMax = dist[i];
+					hMaxIdx = i;
+				}
+			} else {
+				if (wMax < dist[i]) {
+					wMax = dist[i];
+					wMaxIdx = i;
+				}
+			}
+		}
+		int maxSquare = wMax * hMax; 
+		int minSquare = dist[(wMaxIdx + 3) % 6] * dist[(hMaxIdx + 3) % 6]; 
+ 
+		System.out.println((maxSquare - minSquare) * K);
+	}
+ 
+}
