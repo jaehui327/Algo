5ba22402f24ee5db5508f3fd5dcb889544095b55
zhilogs <jaehui327@naver.com>
2023-10-14 20:57:09 +0900

    [Bronze II] Title: 벌집, Time: 80 ms, Memory: 13560 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    static long solution(long n, long cnt, long N) {
+        if (n >= N) return cnt;
+        return solution(n + 6 * cnt, cnt + 1, N);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Integer.parseInt(br.readLine());
+        System.out.println(solution(1, 1, N));
+        br.close();
+    }
+}
