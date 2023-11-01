672eb4ec85be13e56c50a02d2279ab308052f9b8
zhilogs <jaehui327@naver.com>
2023-11-02 01:41:35 +0900

    [Gold V] Title: 0 만들기, Time: 88 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Gold/7490. 0 만들기/0 만들기.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static StringBuilder answer;
+
+    public static void isZero(int i, int num, int sign, int sum, int N, String s) {
+        if (i == N) {
+            sum = sum + (sign * num);
+            if (sum == 0)
+                answer.append(s).append("\n");
+            return;
+        }
+        isZero(i + 1, num * 10 + (i + 1), sign, sum, N, s + " " + (i + 1));
+        isZero(i + 1, i + 1, 1, sum + (sign * num), N, s + "+" + (i + 1));
+        isZero(i + 1, i + 1, -1, sum + (sign * num), N, s + "-" + (i + 1));
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        answer = new StringBuilder();
+
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            isZero(1, 1, 1, 0, N, "1");
+            answer.append("\n");
+        }
+
+        System.out.println(answer);
+        br.close();
+    }
+}
