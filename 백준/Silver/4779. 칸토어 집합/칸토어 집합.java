5d8602fed0d2e11dd0efeac55ddef857a590414c
zhilogs <jaehui327@naver.com>
2023-12-20 00:03:54 +0900

    [Silver III] Title: 칸토어 집합, Time: 208 ms, Memory: 27004 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static char[] answer;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String s;
+        int n;
+        while ((s = br.readLine()) != null) {
+            n = Integer.parseInt(s);
+
+            if (n == 0) sb.append("-").append("\n");
+            else {
+                int size = (int) Math.pow(3, n);
+                answer = new char[size];
+                for (int i = 0; i < size; i++) answer[i] = '-';
+                solution(0, size);
+                sb.append(answer).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static void solution(int start, int n) {
+        if (n == 0) return;
+        for (int i = start + n / 3; i < start + n / 3 * 2; i++) {
+            answer[i] = ' ';
+        }
+        solution(start, n / 3);
+        solution(start + n / 3 * 2, n / 3);
+    }
+}
