f51b183293d2227ebadb68997a332f4fcac79327
zhilogs <jaehui327@naver.com>
2024-01-02 21:45:44 +0900

    [Bronze V] Title: 문자열, Time: 72 ms, Memory: 11304 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            sb.append(s.substring(0, 1)).append(s.substring(s.length() - 1)).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
