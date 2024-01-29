a0eaca1ffeb9e9f909bb31617182ab72a192eb34
zhilogs <jaehui327@naver.com>
2024-01-29 16:52:19 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 616 ms, Memory: 51664 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            String status = st.nextToken();
+            if (status.equals("enter")) set.add(name);
+            else set.remove(name);
+        }
+        ArrayList<String> list = new ArrayList<>(set);
+        Collections.sort(list, Comparator.reverseOrder());
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append("\n");
+        }
+        System.out.println(sb);
+        br.close();
+    }
+}
