9a6c3f81bd791c901572a6ec826b11f5d6c18eed
zhilogs <jaehui327@naver.com>
2024-02-01 10:19:22 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 76 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] prefixSum = new int[10];
+        prefixSum[0] = Integer.parseInt(br.readLine());
+        for (int i = 1; i < 10; i++) prefixSum[i] = prefixSum[i - 1] + Integer.parseInt(br.readLine());
+
+        int result = 0;
+        for (int num : prefixSum) {
+            if (Math.abs(100 - result) < Math.abs(100 - num)) continue;
+            result = num;
+        }
+
+        System.out.println(result);
+        br.close();
+    }
+}
