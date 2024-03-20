d811385233485f58ab29ffa124d1ff3a06c41646
zhilogs <jaehui327@naver.com>
2024-03-20 17:40:09 +0900

    [Silver I] Title: 신입 사원, Time: 1364 ms, Memory: 304928 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int N = Integer.parseInt(br.readLine());
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+                map.put(a, b);
+            }
+            sb.append(cntNewRecruit(N, map)).append("\n");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+
+    public static int cntNewRecruit(int N, HashMap<Integer, Integer> map) {
+        int result = 1;
+        int high = map.get(1);
+
+        for (int i = 2; i <= N; i++) {
+            if (map.get(i) > high) continue;
+            result++;
+            high = map.get(i);
+        }
+
+        return result;
+    }
+
+}
